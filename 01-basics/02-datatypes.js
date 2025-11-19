a0b637655f70bc0e3c48aada82e377a3650c8b63
Sourav Pandey <101590724+unisourav-18@users.noreply.github.com>
2025-11-19 11:36:22 +0000

    datatype conversion intro

--- 01-basics/02-datatypes.js
+++ 01-basics/02-datatypes.js
@@ -1,29 +1,69 @@
-"use strict"; // treat all JS code as newer version
+let score = "sourav"
 
-// alert( 3 + 3) // we are using nodejs, not browser
+//console.log(typeof score);
+//console.log(typeof(score));
 
-console.log(3 
-    +
-     3) // code readability should be high
-
-console.log("Sourav")
-
-
-let name = "sourav"
-let age = 18
-let isLoggedIn = false
-let state;
-
-// number => 2 to power 53
-// bigint
-// string => ""
-// boolean => true/false
-// null => standalone value
-// undefined => 
-// symbol => unique
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 
-// object
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
 
-console.log(typeof undefined); // undefined
-console.log(typeof null); // object
+let isLoggedIn = "Sourav"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+console.log("1" + 2);
+// console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
